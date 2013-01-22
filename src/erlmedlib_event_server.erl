c1afe4257b646bff86c71d867308e2fbe04ac781
Dipesh Patel <dipthegeezer.opensource@gmail.com>
2013-01-22 17:37:01 +0000

    Basic approach

--- src/erlmedlib_event_server.erl
+++ src/erlmedlib_event_server.erl
@@ -32,6 +32,8 @@ init(Args) ->
 handle_call(_Request, _From, State) ->
     {reply, ok, State}.
 
+handle_cast({process_event, Data}, State) ->
+  {noreply, do_process_event(Data, State)};
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
@@ -48,3 +50,42 @@ code_change(_OldVsn, State, _Extra) ->
 %% Internal Function Definitions
 %% ------------------------------------------------------------------
 
+do_process_event({Path, file, create, Cookie, Name}, State) ->
+%% run through exiftool
+%% Send produced data to couchdb
+    State;
+do_process_event({Path, file, delete, Cookie, Name}, State) ->
+%% Find on couchdb using path and Name
+%% Delete from couchdb
+    State;
+%% A file or directory was moved into a watched directory. This event occurs even if the file is simply moved from and to the same directory.
+do_process_event({Path, file, move_to, Cookie, Name}, State) ->
+%% Treat like a create
+    State;
+%% A file or directory was moved from a watched directory. This event occurs even if the file is simply moved from and to the same directory.
+do_process_event({Path, file, move_from, Cookie, Name}, State) ->
+%% Treat like a delete
+    State;
+
+do_process_event({Path, file, modify, Cookie, Name}, State) ->
+%% run through exiftool
+%% Find on couchdb using path and Name
+%% update couchdb
+    State;
+
+do_process_event({Path, dir, create, Cookie, Name}, State) ->
+%% Watch directory
+    State;
+do_process_event({Path, dir, delete, Cookie, Name}, State) ->
+%% Unwatch directory
+    State;
+%% A file or directory was moved to a watched directory. This event occurs even if the file is simply moved from and to the same directory.
+do_process_event({Path, dir, move_to, Cookie, Name}, State) ->
+%% Watch Directory
+    State;
+do_process_event({Path, dir, move_from, Cookie, Name}, State) ->
+%% UnWatch Directory
+    State;
+do_process_event({Path, dir, modify, Cookie, Name}, State) ->
+%% nothing?
+    State.
