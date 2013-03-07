9d9e12b56c839a0e438212f576d73c723289323d
Dipesh Patel <dipthegeezer.opensource@gmail.com>
2013-03-07 17:25:27 +0000

    Finally got some stream sense

--- src/erlmedlib_fileuploader_resource.erl
+++ src/erlmedlib_fileuploader_resource.erl
@@ -17,20 +17,19 @@ allowed_methods(ReqData, Context) ->{['POST'], ReqData, Context}.
 
 process_post(ReqData, State) ->
     Boundary = webmachine_multipart:find_boundary(ReqData),
-    Parts = webmachine_multipart:getparts1(
-              webmachine_multipart:stream_parts(
-                wrq:stream_req_body(ReqData, 1024),Boundary
-              ), []
-            ).
-    io:format("Parts ~p~n",[Parts]),
+    io:format("Boundary ~p~n",[Boundary]),
+    Parts = accumulate_stream_parts(webmachine_multipart:stream_parts(
+                wrq:stream_req_body(ReqData, 1024), Boundary
+              ),[]),
+    io:format("Parts ~p~n", [Parts]),
     NewReqData = wrq:set_resp_header("Content-type", "text/plain", wrq:set_resp_body(json_body([{success, "true"}]), ReqData)),
     {true, NewReqData, State}.
 
-get_streamed_body({Hunk,done},Acc) ->
-    io:format("RECEIVED ~p~n",[Hunk]),
-    iolist_to_binary(lists:reverse([Hunk|Acc]));
-get_streamed_body({Hunk,Next},Acc) ->
-    io:format("RECEIVED ~p~n",[Hunk]),
-    get_streamed_body(Next(),[Hunk|Acc]).
+accumulate_stream_parts(done_parts, Acc) ->
+    %%io:format("RECEIVED ~p~n",[done_parts]),
+    lists:reverse(Acc);
+accumulate_stream_parts({Hunk,Next},Acc) ->
+    %%io:format("RECEIVED ~p~n",[Hunk]),
+    accumulate_stream_parts(Next(),[Hunk|Acc]).
 
 json_body(QS) -> mochijson:encode({struct, QS}).
