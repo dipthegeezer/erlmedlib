f1636517fd3054fc6e3078c8a5ac1d3dab19a17e
Dipesh Patel <dipthegeezer@opensource.gmail.com>
2013-03-10 18:57:44 +0000

    Some tests and a few other bits and pieces.

--- src/erlmedlib_fileuploader_resource.erl
+++ src/erlmedlib_fileuploader_resource.erl
@@ -1,4 +1,29 @@
+%% @author Dipesh Patel <dipthegeezer.opensource(at)gmail.com>
+%% @copyright 2013 Dipesh Patel
+
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%% http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+
+%% @doc Webmachine resource file for use with file-uploader
+%% @reference See <a href="https://github.com/valums/file-uploader">file-uploader</a>
+%% and <a href="https://github.com/basho/webmachine">webmachine</a>
+%% for more information.
+
 -module(erlmedlib_fileuploader_resource).
+
+%% ------------------------------------------------------------------
+%% API Function Exports
+%% ------------------------------------------------------------------
 
 -export([init/1,
          allowed_methods/2,
@@ -7,14 +32,27 @@
 
 -include_lib("webmachine/include/webmachine.hrl").
 
+%% @type context() = Record :: #context{ outdir = filelib:dirname() }.
+
 -record(context, {outdir}).
 
-%%TODO:Docs, tests, check delete works, debug mode to log file.
+%% @todo tests, check delete works, debug mode to log file.
 
+%% ------------------------------------------------------------------
+%% API Function Definitions
+%% ------------------------------------------------------------------
+
+%% @spec init([OutputDir]) -> {ok, context()}
+%%      OutputDir = filelib:dirname()
+%% @doc Initialise resource.
 init([OutputDir]) -> {ok, #context{outdir=OutputDir}}.
 
-allowed_methods(ReqData, Context) ->{['POST', 'DELETE'], ReqData, Context}.
+%% @spec allowed_methods(wrq:reqdata(), context()) -> {[atom()], wrq:reqdata(), context()}
+%% @doc Only allow POST and DELETE
+allowed_methods(ReqData, Context) -> {['POST', 'DELETE'], ReqData, Context}.
 
+%% @spec delete_resource(wrq:reqdata(), context()) -> {boolean(), wrq:reqdata(), context()}
+%% @doc Delete the file given by the uuid in the dispatch path
 delete_resource(ReqData, Context) ->
     Dirname = file_path(wrq:disp_path(ReqData), Context),
     Files = find_all_files(Dirname++"/*"),
@@ -26,6 +64,8 @@ delete_resource(ReqData, Context) ->
         _ -> {false, ReqData, Context}
     end.
 
+%% @spec process_post(wrq:reqdata(), context()) -> {boolean(), wrq:reqdata(), context()}
+%% @doc process a file post request. Assumes files are sent in parts.
 process_post(ReqData, Context) ->
     try
         Boundary = webmachine_multipart:find_boundary(ReqData),
@@ -55,6 +95,12 @@ process_post(ReqData, Context) ->
         %%Maybe clean out file system?
     end.
 
+%% ------------------------------------------------------------------
+%% Internal Function Definitions
+%% ------------------------------------------------------------------
+
+%% @spec accumulate_stream_parts('done_parts' | {fpart(), function()}, [fpart()]) -> [fpart()]
+%% @doc Return all multipart streams as list
 accumulate_stream_parts(done_parts, Acc) ->
     %%io:format("RECEIVED ~p~n",[done_parts]),
     lists:reverse(Acc);
@@ -62,6 +108,8 @@ accumulate_stream_parts({Hunk,Next},Acc) ->
     %%io:format("RECEIVED ~p~n",[Hunk]),
     accumulate_stream_parts(Next(),[Hunk|Acc]).
 
+%% @spec write_to_disk([fpart()], context()) -> ok | {error,Error}
+%% @doc Write part to disk, consolidate parts to one file if receive final part.
 write_to_disk(Parts, Context) ->
     Path = file_path(qquuid(Parts), Context),
     case filelib:ensure_dir(Path++"/") of
@@ -73,6 +121,9 @@ write_to_disk(Parts, Context) ->
             end;
         {error,Error} -> {error,Error}
     end.
+
+%% @spec write_to_disk([fpart()], context(), integer(), integer()) -> ok | {error,Error}
+%% @doc Write a part to disk, consolidate parts to one file if part is last one.
 write_to_disk(Parts, Context, Last, Last) ->
     case write_to_disk(Parts, Context, Last, Last+1) of
         ok ->
@@ -100,8 +151,8 @@ write_to_disk(Parts, Context, Index, _TotalIndex) ->
     end.
 
 
-%%write parts to one file
-%%returns ok | {error, Reason}
+%% @spec write_combined_parts(file:filename()) -> ok | {error,Error}
+%% @doc Write parts to one file
 write_combined_parts(Filename) when is_list(Filename) ->
     Files = find_all_files(Filename++"_*"),
     %%io:format("Com files ~p~n",[Files]),
@@ -116,6 +167,9 @@ write_combined_parts(Filename) when is_list(Filename) ->
             end;
         {error, Reason} -> {error, Reason}
     end.
+
+%% @spec write_combined_parts(file:iodata(), [file:filename()]) -> ok | {error,Error}
+%% @doc Write parts to one file
 write_combined_parts(Handle,[]) ->
     file:close(Handle);
 write_combined_parts(Handle, [H|T]) ->
@@ -153,7 +207,6 @@ get_param(Name, Parts) ->
     Val.
 
 %%responses
-
 success(ReqData) ->
     success("true", ReqData).
 
@@ -182,16 +235,23 @@ response_body(Status, ReqData) ->
       ), ReqData
     ).
 
+%% @spec binary_to_int(binary()) -> integer()
 binary_to_int(N) ->
     list_to_integer(binary_to_list(N)).
 
+%% @spec check_file_size(integer(), integer()) -> ok | {error, string()}
+%% @doc Check that the two sizes given match
 check_file_size(_Same, _Same) -> ok;
 check_file_size(FileSize, Expected) ->
     {error, "Expected "++Expected++" got "++FileSize}.
 
+%% @spec find_all_files(file:filename()) -> [file:filename()]
+%% @doc find all files of the given wildcard
 find_all_files(Wildcard) ->
     lists:sort(filelib:wildcard(Wildcard)).
 
+%% @spec delete_files([file:filename()]) -> ok | {error,Error}
+%% @doc delete files from given list
 delete_files([]) -> ok;
 delete_files([H|T]) ->
     case file:delete(H) of
@@ -199,6 +259,8 @@ delete_files([H|T]) ->
         {error, Reason} -> {error, Reason}
     end.
 
+%% @spec file_path(string(), context()) -> false | file:filename()
+%% @doc Removes any begining "/" from the Name.
 file_path([], _Context) ->
     false;
 file_path(Name, Context) ->
@@ -207,3 +269,40 @@ file_path(Name, Context) ->
         _ -> Name
     end,
     filename:join([Context#context.outdir, RelName]).
+
+
+
+%% ------------------------------------------------------------------
+%% EUnit Tests
+%% ------------------------------------------------------------------
+-ifdef(TEST).
+
+-include_lib("eunit/include/eunit.hrl").
+
+setup() ->
+    [{"qqfilename", some_stuff, <<"test.txt">>},
+     {"qqtotalparts", some_stuff, <<"2">>},
+     {"qqtotalfilesize", some_stuff, <<"10">>},
+     {"qqpartindex", some_stuff, <<"1">>},
+     {"qquuid", some_stuff, <<"er3-rere-dc">>},
+     {"qqfile", some_stuff, <<"binary data for the win">>}
+    ].
+
+cleanup(_SetupData) ->
+    ok.
+
+params_test_() ->
+    {setup,
+     fun setup/0,
+     fun cleanup/1,
+     fun(SetupData) ->
+             ?_assertEqual("test.txt", qqfilename(SetupData)),
+             ?_assertEqual(2, qqtotalparts(SetupData)),
+             ?_assertEqual(10, qqtotalfilesize(SetupData)),
+             ?_assertEqual(1, qqpartindex(SetupData)),
+             ?_assertEqual("er3-rere-dc", qquuid(SetupData)),
+             ?_assertEqual(<<"binary data for the win">>, qqfile(SetupData))
+     end
+    }.
+
+-endif.
